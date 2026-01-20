25537d4ce9928a9db11f31140ad39ad9daca11a3
Vinicius Zorzella <vlczorzella@hotmail.com>
2026-01-19 21:24:41 -0400

    renomeando colunas de imagem

--- backend/artists-api/src/main/resources/db/migration/V1__criar_tabelas_artista_album.sql
+++ backend/artists-api/src/main/resources/db/migration/V1__criar_tabelas_artista_album.sql
@@ -1,14 +1,14 @@
 CREATE TABLE artistas (
                           id SERIAL PRIMARY KEY,
                           nome VARCHAR(200) NOT NULL,
-                          foto VARCHAR(255),
+                          foto_url VARCHAR(255),
                           criado_em TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE TABLE albuns (
                        id SERIAL PRIMARY KEY,
                        titulo VARCHAR(200) NOT NULL,
-                       capa VARCHAR(255),
+                       capa_url VARCHAR(255),
                        artista_id INTEGER NOT NULL,
                        criado_em TIMESTAMP NOT NULL DEFAULT now(),
                        CONSTRAINT fk_album_artista FOREIGN KEY (artista_id) REFERENCES artistas(id)
